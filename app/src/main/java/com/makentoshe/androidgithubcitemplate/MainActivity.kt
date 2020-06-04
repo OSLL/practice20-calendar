fedb2adc5540aad225bfa227f30ae8d1d42a880c
Ivan Ilichev <swggNuko@gmail.com>
2020-06-04 23:54:12 +0300

    Some comments

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -9,6 +9,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    //Дата, месяц и год
+
     public var date : Int = 0
     public var month : Int = 0
     public var year : Int = 0
@@ -16,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Клик по дате в календаре
         calendar_view.setOnDateChangeListener(object : CalendarView.OnDateChangeListener {
             override fun onSelectedDayChange(p0: CalendarView, p1: Int, p2: Int, p3: Int) {
                 date = p3
@@ -24,8 +28,16 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        // get reference to button
+        // Клик по кнопке добалвения события
         fab.setOnClickListener {
+            /*
+                
+                Код открытия формы добавления события
+                ! Дата, месяц и год заполняются автоматически из меременных date, month, year, которые выставляются при клике на дату в календаре
+
+             */
+
+
             Toast.makeText(this, "Button click. The selected date is $date. $month .$year", Toast.LENGTH_LONG).show()
         }
     }
