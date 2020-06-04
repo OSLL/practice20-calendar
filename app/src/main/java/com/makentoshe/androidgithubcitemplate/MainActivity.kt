14adb2c410b9c4de0e4aca4e406d55c2f4cf435e
Ivan Ilichev <swggNuko@gmail.com>
2020-06-04 23:45:39 +0300

    Plus button

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -4,19 +4,33 @@ import android.os.Bundle
 import android.widget.CalendarView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Button
+
 class MainActivity : AppCompatActivity() {
+    public var date : Int = 0
+    public var month : Int = 0
+    public var year : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         calendar_view.setOnDateChangeListener(object : CalendarView.OnDateChangeListener {
             override fun onSelectedDayChange(p0: CalendarView, p1: Int, p2: Int, p3: Int) {
-                Toast.makeText(
-                    this@MainActivity,
-                    "The selected date is $p3.${p2 + 1}.$p1",
-                    Toast.LENGTH_SHORT
-                ).show()
+                date = p3
+                month = p2 + 1
+                year = p1
             }
         })
+
+        // get reference to button
+        fab.setOnClickListener {
+            Toast.makeText(this, "Button click. The selected date is $date. $month .$year", Toast.LENGTH_LONG).show()
+        }
     }
+
+
 }
+
+
