99574946326e9f52e44f86f95d2c399a1b4c28c1
Ivan Ilichev <swggNuko@gmail.com>
2020-06-04 13:58:13 +0300

    Calendar layout

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.makentoshe.androidgithubcitemplate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.CalendarView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        calendar_view.setOnDateChangeListener(object : CalendarView.OnDateChangeListener {
+            override fun onSelectedDayChange(p0: CalendarView, p1: Int, p2: Int, p3: Int) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "The selected date is $p3.${p2 + 1}.$p1",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
     }
 }
