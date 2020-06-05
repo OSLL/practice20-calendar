4b41c0b495964462c05bd757a4d03c1be6a8701a
Ivan Ilichev <swggNuko@gmail.com>
2020-06-05 19:28:34 +0300

    Return to current date

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.makentoshe.androidgithubcitemplate
 
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils.loadAnimation
+import android.widget.Button
 import android.widget.CalendarView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.view.animation.AnimationUtils
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
-import com.makentoshe.androidgithubcitemplate.EventActivity
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         // По умолчанию выставляются текущие дата, месяц, год
         val c = Calendar.getInstance()
         date = c.get(Calendar.DATE);
-        month = c.get(Calendar.MONTH);
+        month = c.get(Calendar.MONTH)+1;
         year = c.get(Calendar.YEAR);
     }
 
@@ -33,22 +36,89 @@ class MainActivity : AppCompatActivity() {
         calendar_view.setOnDateChangeListener(object : CalendarView.OnDateChangeListener {
             override fun onSelectedDayChange(p0: CalendarView, p1: Int, p2: Int, p3: Int) {
                 if (date == p3 && month==p2 + 1 && year == p1) {
-                    
+
+                    /*
+
+                    Модуль по открытию страницы событий на дату
+
+                     */
+
+                    Toast.makeText(applicationContext, "Double click", Toast.LENGTH_LONG).show()
                 }
 
                 date = p3
                 month = p2 + 1
                 year = p1
+
+                //Показывает либо скрывает кнопку возвращения к текущей дате
+                retrunButtonHideShow(date,month,year)
             }
         })
 
+        //Возврат к текущей дате
+        returnToCurrent.setOnClickListener{
+
+            val c = Calendar.getInstance()
+
+            date = c.get(Calendar.DATE)
+            month = c.get(Calendar.MONTH);
+            year = c.get(Calendar.YEAR);
+
+            val calendar = Calendar.getInstance()
+            calendar[Calendar.YEAR] = year
+            calendar[Calendar.MONTH] = month
+            calendar[Calendar.DAY_OF_MONTH] = date
+
+            val milliTime = calendar.timeInMillis
+
+            calendar_view.setDate (milliTime, true, true);
+            hideView()
+        }
+
         // Клик по кнопке добалвения события
-        fab.setOnClickListener {
+        addEvent.setOnClickListener {
+
             val event = EventActivity()
             event.createEvent(date, month, year)
 
-            Toast.makeText(this, "Button click. The selected date is $date. $month .$year", Toast.LENGTH_LONG).show()
         }
+    }
+
+    public fun retrunButtonHideShow(d:Int,m:Int,y:Int) {
+        if (!ifCurrentDate(d,m,y)) {
+            showView()
+        } else {
+            hideView()
+        }
+    }
+
+    //Проверка даты на совпадение с текущей
+    public fun ifCurrentDate(d:Int,m:Int,y:Int) : Boolean {
+        val c = Calendar.getInstance()
+
+        if (c.get(Calendar.DATE) == d && m == c.get(Calendar.MONTH)+1 && y == c.get(Calendar.YEAR)) {
+            return true
+        }
+        return false
+    }
+
+    public fun showView() {
+        var b : View = findViewById<FloatingActionButton>(R.id.returnToCurrent)
+        var popup = AnimationUtils.loadAnimation(this, R.anim.scale_up);
+        if (b.visibility == View.INVISIBLE) {
+            b.startAnimation(popup);
+            b.visibility = View.VISIBLE
+        }
+    }
+
+    public fun hideView() {
+        var b : View = findViewById<FloatingActionButton>(R.id.returnToCurrent)
+        var popdown = AnimationUtils.loadAnimation(this, R.anim.scale_down);
+        if (b.visibility == View.VISIBLE) {
+            b.startAnimation(popdown);
+            b.visibility = View.INVISIBLE
+        }
+
     }
 
 
